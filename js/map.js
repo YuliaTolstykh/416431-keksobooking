456845da8ae273c9ad7c3e14ff9ec48b689c764a
YuliaTolstykh <yuliatolstykh@yandex.ru>
2017-11-25 14:44:14 +0300

    Решаю конфликты

--- js/map.js
+++ js/map.js
@@ -1,12 +1,14 @@
+'use strict';
+
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var types = ['flat', 'house', 'bungalo'];
-var times = ['12:00','13:00', '14:00'];
+var times = ['12:00', '13:00', '14:00'];
 // var attributes = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var getLength = function(arr) {
-  value = arr.length;
+var getLength = function (arr) {
+  var value = arr.length;
   arr.length = getRandomInt(0, value);
   return arr;
 };
@@ -15,26 +17,27 @@ var advert = [];
 for (var i = 0; i <= 8; i++) {
   var attributes = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
   advert[i] = {
-    'author': 'img/avatars/user{{0' + getRandomInt(0,8) + '}}.png',
+    'author': 'img/avatars/user{{0' + getRandomInt(0, 8) + '}}.png',
     'offer': {
-      'title': titles[getRandomInt(0,8)],
+      'title': titles[getRandomInt(0, 8)],
       'address': '{{location.x}}, {{location.y}}',
-      'price': getRandomInt(1000,1000000),
-      'type': types[getRandomInt(0,2)],
-      'rooms': getRandomInt(1,5),
-      'guests': getRandomInt(1,100),
-      'checkin': times[getRandomInt(0,2)],
-      'checkout': times[getRandomInt(0,2)],
+      'price': getRandomInt(1000, 1000000),
+      'type': types[getRandomInt(0, 2)],
+      'rooms': getRandomInt(1, 5),
+      'guests': getRandomInt(1, 100),
+      'checkin': times[getRandomInt(0, 2)],
+      'checkout': times[getRandomInt(0, 2)],
       'features': getLength(attributes),
       'description': '',
       'photos': []
     },
-    'location':{
-      'x': getRandomInt(300,900),
-      'y': getRandomInt(100,500)
+    'location': {
+      'x': getRandomInt(300, 900),
+      'y': getRandomInt(100, 500)
     }
   };
 }
+console.log(advert);
 console.log(advert);
 var aA = document.querySelector('.map');
 aA.classList.remove('map--faded');
