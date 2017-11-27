bc4ab714496ca204565f4c6ff4a1f9baf873c1f7
YuliaTolstykh <yuliatolstykh@yandex.ru>
2017-11-27 22:02:11 +0300

    Задача 5 продолжение

--- js/map.js
+++ js/map.js
@@ -3,25 +3,35 @@
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
+var sample = function (vector, n) {
+  var number = vector.splice(getRandomInt(0, n), 1);
+  return number;
+};
 var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var types = ['flat', 'house', 'bungalo'];
 var times = ['12:00', '13:00', '14:00'];
-// var attributes = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var getLength = function (arr) {
-  var value = arr.length;
-  arr.length = getRandomInt(0, value);
-  return arr;
+  var value = arr.length + 1;
+  var attributes = [];
+  var pp = [];
+  var p = 0;
+  do {
+    pp[p] = getRandomInt(1, value);
+    attributes[p] = sample(arr, value - (1 + p));
+    p++;
+  } while (p < pp[0]);
+  return attributes;
 };
-// var k = [1, 2, 3, 4, 5, 6, 7, 8];
+var k = [1, 2, 3, 4, 5, 6, 7, 8];
 var advert = [];
-for (var i = 0; i < 8; i++) {
-  var attributes = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+for (var i = 7; i >= 0; i--) {
+  var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
   advert[i] = {
     'author': {
-      'avatar': 'img/avatars/user0' + getRandomInt(1, 9) + '.png'
+      'avatar': 'img/avatars/user0' + sample(k, i) + '.png'
     },
     'offer': {
-      'title': titles[getRandomInt(0, 8)],
+      'title': sample(titles, i),
       'address': 'location.x, location.y',
       'price': getRandomInt(1000, 1000000),
       'type': types[getRandomInt(0, 3)],
@@ -29,7 +39,7 @@ for (var i = 0; i < 8; i++) {
       'guests': getRandomInt(1, 100),
       'checkin': times[getRandomInt(0, 3)],
       'checkout': times[getRandomInt(0, 3)],
-      'features': getLength(attributes),
+      'features': getLength(features),
       'description': '',
       'photos': []
     },
@@ -58,7 +68,6 @@ for (var j = 0; j < advert.length; j++) {
 }
 advertItem.parentElement.appendChild(fragment);
 var similarAdvertTemplate = document.querySelector('template').content;
-console.log(advert[0].offer.type);
 var advertElement = similarAdvertTemplate.cloneNode(true);
 advertElement.querySelector('h3').textContent = advert[0].offer.title;
 advertElement.querySelector('p').textContent = advert[0].offer.address;
@@ -78,7 +87,14 @@ var offerType = function () {
 advertElement.querySelector('h4').textContent = offerType();
 advertElement.querySelectorAll('p')[2].textContent = advert[0].offer.rooms + ' для ' + advert[0].offer.guests + ' гостей';
 advertElement.querySelectorAll('p')[3].textContent = 'Заезд после ' + advert[0].offer.checkin + ', выезд до ' + advert[0].offer.checkout;
-// advertElement.querySelector('.popup__features').innerHTML = advert[0].offer.features;
+var featuresLength = advert[0].offer.features;
+var textHTML = [];
+var n = 0;
+while (n < featuresLength.length) {
+  textHTML[n] = '<li class="feature feature--' + featuresLength[n] + '"></li>';
+  n++;
+}
+advertElement.querySelector('.popup__features').innerHTML = textHTML.join('');
 advertElement.querySelectorAll('p')[4].textContent = advert[0].offer.description;
 advertElement.querySelector('.popup__avatar').setAttribute('src', advert[0].author.avatar);
 map.appendChild(advertElement);
