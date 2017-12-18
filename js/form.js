9bb229c3b500f4dab0353e395a631081cd299cb8
YuliaTolstykh <yuliatolstykh@yandex.ru>
2017-12-18 13:38:09 +0300

    Еще раз исправила синхронизацию

--- js/form.js
+++ js/form.js
@@ -63,10 +63,61 @@
     returnColor(titleInput);
     returnColor(priceInput);
   });
-  window.synchronizeFields(selectTimein, selectTimeout, '', '', window.syncValues);
-  window.synchronizeFields(selectTimeout, selectTimein, '', '', window.syncValues);
-  window.synchronizeFields(selectType, priceInput, minPricePerNight, apartmentType, window.syncValueWithMin);
-  window.synchronizeFields(selectRooms, selectCapacity, '', '', window.syncValueWithPersons);
+  var syncValues = function (field1, field2) {
+    field2.options[field1.selectedIndex].selected = true;
+  };
+  var syncValueWithMin = function (field1, field2, value1, value2) {
+    if (field1.selectedIndex === 0) {
+      field2.value = value1[0];
+      field2.min = value1[0];
+      minPriceMessage = value2[0] + ' ' + value1[0];
+    } else if (field1.selectedIndex === 1) {
+      field2.value = value1[1];
+      field2.min = value1[1];
+      minPriceMessage = value2[1] + ' ' + value1[1];
+    } else if (field1.selectedIndex === 2) {
+      field2.value = value1[2];
+      field2.min = value1[2];
+      minPriceMessage = value2[2] + ' ' + value1[2];
+    } else if (field1.selectedIndex === 3) {
+      field2.value = value1[3];
+      field2.min = value1[3];
+      minPriceMessage = value2[3] + ' ' + value1[3];
+    }
+    return minPriceMessage;
+  };
+  var syncValueWithPersons = function (field1, field2) {
+    if (field1.selectedIndex === 0) {
+      field2.options[2].selected = true;
+      field2.options[0].setAttribute('disabled', 'disabled');
+      field2.options[1].setAttribute('disabled', 'disabled');
+      field2.options[2].removeAttribute('disabled', 'disabled');
+      field2.options[3].setAttribute('disabled', 'disabled');
+    } else if (field1.selectedIndex === 1) {
+      field2.options[1].selected = true;
+      field2.options[0].setAttribute('disabled', 'disabled');
+      field2.options[1].removeAttribute('disabled', 'disabled');
+      field2.options[2].removeAttribute('disabled', 'disabled');
+      field2.options[3].setAttribute('disabled', 'disabled');
+    } else if (field1.selectedIndex === 2) {
+      field2.options[0].selected = true;
+      field2.options[0].removeAttribute('disabled', 'disabled');
+      field2.options[1].removeAttribute('disabled', 'disabled');
+      field2.options[2].removeAttribute('disabled', 'disabled');
+      field2.options[3].setAttribute('disabled', 'disabled');
+    } else if (field1.selectedIndex === 3) {
+      field2.options[3].selected = true;
+      field2.options[0].setAttribute('disabled', 'disabled');
+      field2.options[1].setAttribute('disabled', 'disabled');
+      field2.options[2].setAttribute('disabled', 'disabled');
+      field2.options[3].removeAttribute('disabled', 'disabled');
+    }
+  };
+  syncValueWithPersons(selectRooms, selectCapacity);
+  window.synchronizeFields(selectTimein, selectTimeout, '', '', syncValues);
+  window.synchronizeFields(selectTimeout, selectTimein, '', '', syncValues);
+  window.synchronizeFields(selectType, priceInput, minPricePerNight, apartmentType, syncValueWithMin);
+  window.synchronizeFields(selectRooms, selectCapacity, '', '', syncValueWithPersons);
   form.addEventListener('submit', function (event) {
     if (titleInput.value.length < minLengthTitle || titleInput.value.length > maxLengthTitle) {
       changeColor(titleInput);
